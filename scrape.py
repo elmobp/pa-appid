1f44ff0134932c08f8ce44276c485c745544ae2f
Michael Baker <lidder86@gmail.com>
2023-05-17 12:06:54 +0800

    done

--- scrape.py
+++ scrape.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import requests
-import re
+import json
+from bs4 import BeautifulSoup
 
 url = "https://applipedia.paloaltonetworks.com/Home/GetApplicationListView"
 data = {
@@ -15,9 +16,27 @@ data = {
 response = requests.post(url, data=data)
 content = response.text
 
-matches = re.findall(r"ShowApplicationDetail\('\d+', '(.*?)', '\d+'\);", content, re.MULTILINE)
-output = '\n'.join(matches)
+soup = BeautifulSoup(content, 'html.parser')
+table = soup.find(id="bodyScrollingTable")
+child_elements = table.find_all('tr')
+
+services = {}
+
+for i in range(len(child_elements)):
+    service_strip = child_elements[i].find_all('td')[0].get_text().strip().replace(' ', '')
+    services[service_strip] = []
+
+    elem_size = int(child_elements[i].get('ottawagroup'))
+
+    if elem_size == 1:
+        b = i + 1
+        while int(child_elements[b].get('ottawagroup')) == 2:
+            service = child_elements[b].find_all('td')[0].get_text().strip().replace(' ', '')
+            services[service_strip].append(service)
+            b += 1
+
+j = json.dumps(services, indent=4)
 
 with open("appid.txt", "w") as file:
-    file.write(output)
+    file.write(j)
 
