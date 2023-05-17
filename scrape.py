a79af69faf162daea93468938b9cd4245bdf9ae9
Michael Baker <lidder86@gmail.com>
2023-05-17 12:49:05 +0800

    New scrape

--- scrape.py
+++ scrape.py
@@ -16,27 +16,56 @@ data = {
 response = requests.post(url, data=data)
 content = response.text
 
-soup = BeautifulSoup(content, 'html.parser')
+soup = BeautifulSoup(content, "html.parser")
 table = soup.find(id="bodyScrollingTable")
-child_elements = table.find_all('tr')
+child_elements = table.find_all("tr")
 
 services = {}
 
+# Iterate over the table rows
 for i in range(len(child_elements)):
-    service_strip = child_elements[i].find_all('td')[0].get_text().strip().replace(' ', '')
-    services[service_strip] = []
+    td_elements = child_elements[i].find_all("td")
 
-    elem_size = int(child_elements[i].get('ottawagroup'))
+    service_strip = td_elements[0].get_text().strip().replace(" ", "")
+    category = td_elements[1].get_text().strip().replace(" ", "")
+    subcategory = td_elements[2].get_text().strip().replace(" ", "")
 
+    services[service_strip] = {}
+    if td_elements[3].find("img"):
+        risk = td_elements[3].find("img")["title"]
+        services[service_strip]["risk"] = risk
+
+    technology = td_elements[4].get_text().strip().replace(" ", "")
+    services[service_strip]["category"] = category
+    services[service_strip]["subcategory"] = subcategory
+    services[service_strip]["technology"] = technology
+
+    b = i
+    elem_size = int(child_elements[i].get("ottawagroup"))
     if elem_size == 1:
-        b = i + 1
-        while int(child_elements[b].get('ottawagroup')) == 2:
-            service = child_elements[b].find_all('td')[0].get_text().strip().replace(' ', '')
-            services[service_strip].append(service)
+        services[service_strip]["subapplications"] = {}
+        b += 1
+        while int(child_elements[b].get("ottawagroup")) == 2:
+            name = child_elements[b].find_all("td")[0].get_text().strip().replace(" ", "")
+            category = child_elements[b].find_all("td")[1].get_text().strip().replace(" ", "")
+            subcategory = child_elements[b].find_all("td")[2].get_text().strip().replace(" ", "")
+            service = {}
+
+            if child_elements[b].find_all("td")[3].find("img"):
+                risk = child_elements[b].find_all("td")[3].find("img")["title"]
+                service["risk"] = risk
+
+            technology = child_elements[b].find_all("td")[4].get_text().strip().replace(" ", "")
+            service["category"] = category
+            service["subcategory"] = subcategory
+            service["technology"] = technology
+
+            services[service_strip]["subapplications"][name] = [service]
             b += 1
 
+# Convert services dictionary to JSON
 j = json.dumps(services, indent=4)
 
-with open("appid.txt", "w") as file:
-    file.write(j)
-
+# Write JSON to file
+with open("appid.txt", "w") as fh:
+    fh.write(j)
